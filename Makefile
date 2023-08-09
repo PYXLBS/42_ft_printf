92dbaa1b1eca1652c12527c3310e5c734a924f4b
pyxlbs <polaberto@protonmail.com>
2023-08-09 14:00:13 +0200

    *

--- Makefile
+++ Makefile
@@ -6,7 +6,37 @@
 #    By: pabertha <pabertha@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/08/09 13:40:16 by pabertha          #+#    #+#              #
-#    Updated: 2023/08/09 13:40:17 by pabertha         ###   ########.fr        #
+#    Updated: 2023/08/09 14:00:00 by pabertha         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME = libftprintf.a
+
+SRC = ft_vtoi.c \
+	ft_convert.c \
+	ft_printf.c \
+
+CC = gcc
+
+CFLAGS = -Wall -Wextra -Werror
+
+OBJ = $(SRC:.c=.o)
+
+all : $(NAME)
+	norminette -R CheckForbiddenSourceHeader *.c
+
+$(NAME) : $(OBJ)
+		ar rcs $(NAME) $(OBJ)
+
+%.o : %.c printf.h
+	$(CC) $(CFLAGS) -c $< -o $@
+	
+clean :
+	rm -f $(OBJ)
+
+fclean : clean
+		rm -f $(NAME)
+
+re : fclean all
+
+.PHONY : all bonus clean fclean re
--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: pabertha <pabertha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 16:22:31 by pabertha          #+#    #+#             */
-/*   Updated: 2023/08/09 13:50:30 by pabertha         ###   ########.fr       */
+/*   Updated: 2023/08/09 13:57:17 by pabertha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@
 # include <stdarg.h>
 
 int					ft_printf(const char *content, ...);
-int					ft_strlen(const char *s);
 int					ft_putchar(char c);
+int					ft_strlen(const char *s);
 int					ft_putstr(char *s);
 int					ft_putnbr(int n);
-char				*ft_strchr(const char *s, int c);
 int					ft_putuint(unsigned int n);
 int					ft_puthex(unsigned int n, char c);
 int					ft_putptr_hex(void *ptr);
 int					ft_convert(char flag, va_list arg);
+char				*ft_strchr(const char *s, int c);
 int					ft_handle_content(const char **content);
 
 #endif
